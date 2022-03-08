a8ef6d9a45d9477ff30a4740b563ee51303ec33b
Adamsin36 <97946100+Adamsin36@users.noreply.github.com>
2022-03-08 09:59:30 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let distance = 0
 let snelheid = 100
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
@@ -13,4 +14,14 @@ basic.forever(function () {
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, snelheid)
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, snelheid)
     }
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    distance = Math.idiv(pins.pulseIn(DigitalPin.P1, PulseValue.High), 58)
+    basic.showNumber(distance)
+    basic.pause(100)
 })
